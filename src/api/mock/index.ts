56733b8d1a013a5f5c2328e2dde0a470cfc33ddd
lixiangyu <654762937@qq.com>
2021-12-02 16:29:35 +0800

    ci: http eslint

--- src/api/mock/index.ts
+++ src/api/mock/index.ts
@@ -8,9 +8,8 @@ mock.interceptors.response.use(
   (res: AxiosResponse) => {
     if (res.status === 200) {
       return res.data
-    } else {
-      return Promise.reject(res)
     }
+    return Promise.reject(res)
   },
   (err: AxiosError) => {
     return Promise.reject(err)
