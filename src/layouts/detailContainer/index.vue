3593de4849dad11f1e763c36549664203eca461e
lixiangyu <654762937@qq.com>
2021-12-07 17:35:58 +0800

    ci: detailContainer 样式

--- src/layouts/detailContainer/index.vue
+++ src/layouts/detailContainer/index.vue
@@ -1,8 +1,10 @@
 <template>
   <el-card class="jc-detail-card">
     <template #header>
-      <el-icon><arrow-left-bold /></el-icon>
-      <span class="jc-detail-card-title">详情</span>
+      <div class="jc-detail-card-title">
+        <el-icon @click="back"><arrow-left-bold /></el-icon>
+        <span>详情</span>
+      </div>
     </template>
     <router-view v-slot="{ Component }">
       <keep-alive>
@@ -14,7 +16,7 @@
 
 <script lang="ts">
 import { defineComponent, ref, watchEffect } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { ArrowLeftBold } from '@element-plus/icons'
 export default defineComponent({
   name: 'DetailContainer',
@@ -28,7 +30,22 @@ export default defineComponent({
       // console.log('watchEffect', route.params)
       detailId.value = route.params.id
     })
-    return { detailId }
+
+    const router = useRouter()
+    const back = () => {
+      router.back()
+    }
+    return { detailId, back }
   }
 })
 </script>
+<style lang="scss" scoped>
+.el-icon {
+  color: $jc-text-color-secondary;
+  font-size: inherit;
+  margin-right: $jc-default-dis * 0.5;
+  position: relative;
+  top: 2px;
+  cursor: pointer;
+}
+</style>
